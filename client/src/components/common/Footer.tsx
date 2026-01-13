1c938e684b98a43559adf88fe73d9521306e043f
sanjeebandas <sanjeeban@learning-crew.com>
2026-01-13 16:17:13 +0530

    fixed footer logo and text alignment

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-[#111111] text-white px-6 py-10 text-sm">
       <div className="max-w-[1248px] mx-auto flex flex-col gap-6">
         {/* Logo Section */}
-        <div>
+        <div className="-ml-4">
           <img src="imgs/Frame 362.png" alt="Video Crew" className="h-14" />
         </div>
 
